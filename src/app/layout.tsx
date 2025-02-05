37d2ca3e8857688a5cc72b74c5b31d6b028ddbf5
yaelmeyer <82927426+yaelmeyer@users.noreply.github.com>
2025-02-05 01:42:42 -0300

    sidebar basico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SideBarPage from '../components/sidebar';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +27,13 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        >
+        <div className="flex h-screen">
+          <SideBarPage/>
+          <div className="flex-1 p-4">
+            {children}
+          </div>
+        </div>
       </body>
     </html>
   );
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function SideBarPage() {
+  return (
+    <nav className="top-4 left-4 h-full bg-gray-800 w-64 shadow-lg">
+        <h2 className="text-xl font-bold">Menu</h2>
+        <ul className="mt-4 space-y-4">
+            <li>
+                <Link href="/targetas" className="block p-2 rounded bg-gray-700 hover:bg-gray-200">Targetas</Link>
+            </li>
+            <li>
+                <Link href="/formularios/newgastoc" className="block p-2 rounded bg-gray-700 hover:bg-gray-200">Nuevo Gasto</Link>
+            </li>
+        </ul>
+    </nav>
+  );
+}
